d44001afdd5cca7f7e399480b45e792f6238df77
Mikhail Efremov <jakutenshi@gmail.com>
2023-01-01 22:38:58 +0600

    Project is created

---
+++ src/test/java/com/github/jakutenshi/tacoshop/TacoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jakutenshi.tacoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
