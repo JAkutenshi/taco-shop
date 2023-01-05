8d1563d6cb5210dec38c6eea7ad3b6c6a82a3d21
Mikhail Efremov <jakutenshi@gmail.com>
2023-01-05 23:43:59 +0600

    Validation from section 2.3 close to be finished

--- src/main/java/com/github/jakutenshi/tacoshop/DesignTacoController.java
+++ src/main/java/com/github/jakutenshi/tacoshop/DesignTacoController.java
@@ -3,10 +3,12 @@ package com.github.jakutenshi.tacoshop;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 
 import static com.github.jakutenshi.tacoshop.Ingredient.Type;
@@ -15,6 +17,7 @@ import static com.github.jakutenshi.tacoshop.Ingredient.Type;
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
+    private final static String TACO_DESIGN_VIEW_NAME = "design";
 
     @GetMapping
     public String showDesignForm(Model model) {
@@ -41,11 +44,14 @@ public class DesignTacoController {
         // Добавляем объект, в котором будет результат формы из View
         model.addAttribute(RESULT_TACO_DESIGN_ATTR_NAME, new TacoDesign());
         // Возвращаем пользователю View с именем "design", заполнив предварительно объект model
-        return "design";
+        return TACO_DESIGN_VIEW_NAME;
     }
 
     @PostMapping
-    public String processDesign(TacoDesign design) {
+    public String processDesign(@Valid TacoDesign design, Errors errors) {
+        if (errors.hasErrors()) {
+            return TACO_DESIGN_VIEW_NAME;
+        }
         // ToDo: Здесь будет сохранение рецепта в БД
         log.info("Processing the design: " + design);
         return "redirect:/orders/current";
--- src/main/java/com/github/jakutenshi/tacoshop/OrderController.java
+++ src/main/java/com/github/jakutenshi/tacoshop/OrderController.java
@@ -3,23 +3,31 @@ package com.github.jakutenshi.tacoshop;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
 
+    private final static String ORDER_FORM_VIEW_NAME = "orderForm";
+
     @GetMapping("current")
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
-        return "orderForm";
+        return ORDER_FORM_VIEW_NAME;
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return ORDER_FORM_VIEW_NAME;
+        }
         // ToDo: сохраняем заказ в БД
         log.info("Processing order: " + order);
         return "redirect:/";
